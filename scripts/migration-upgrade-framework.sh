33c2ae1b131ac7c3dfe83705b8fd5655de13bd93
Richard Melkonian <r.v.melkonian@gmail.com>
2025-09-10 22:52:40 +0100

    chore: update upgrade script

--- scripts/migration-upgrade-framework.sh
+++ scripts/migration-upgrade-framework.sh
@@ -3,3 +3,5 @@
 ## First compile the feature flag upgrade script
 ## We need to compile it for the CLI to run it on version 6, atleast, I had to locally.
 movement move compile --package-dir movement-migration/framework-upgrades --bytecode-version 6
+
+movement move run-script --compiled-script-path movement-migration/framework-upgrades/build/feature-flag-reconfig/bytecode_scripts/main.mv
